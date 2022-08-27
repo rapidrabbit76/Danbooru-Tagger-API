8edf92b7551184bde8769191b3a64fedb6f4a917
yslee <yslee.dev@gmail.com>
2022-08-27 23:13:12 +0900

    feat: add router

---
+++ app/routers/danbooru.py
@@ -0,0 +1,76 @@
+import typing as T
+from fastapi import (
+    Depends,
+    File,
+    UploadFile,
+    HTTPException,
+    status,
+    logger,
+    Body,
+)
+from fastapi_restful.cbv import cbv
+
+from fastapi_restful.inferring_router import InferringRouter
+from fastapi.logger import logger
+from app.settings import get_settings
+from app.services import DanbooruService
+
+from PIL import Image
+from class_name import CLASSES
+
+router = InferringRouter()
+setting = get_settings()
+
+
+@cbv(router)
+class Danbooru:
+    svc: DanbooruService = Depends()
+
+    @router.post("/predict/embedding", response_model=T.List[float])
+    def predict_embedding(
+        self,
+        image: UploadFile = File(...),
+    ):
+        logger.info("------------- Tagger Start -----------")
+        image = self.imread(image)
+        output = self.svc.predict_embedding(image)
+        logger.info("------------- Tagger Done -----------")
+        return output
+
+    @router.post("/predict/socre", response_model=T.List[float])
+    def predict_score(
+        self,
+        image: UploadFile = File(...),
+    ):
+        logger.info("------------- Tagger Start -----------")
+        image = self.imread(image)
+        output = self.svc.predict_score(image)
+        logger.info("------------- Tagger Done -----------")
+        return output
+
+    @router.post("/predict/tag", response_model=T.List[str])
+    def predict_tag(
+        self,
+        image: UploadFile = File(...),
+        threshold: float = Body(0.2, embed=True),
+    ):
+        logger.info("------------- Tagger Start -----------")
+        image = self.imread(image)
+        output = self.svc.predict_tags(image, threshold)
+        logger.info("------------- Tagger Done -----------")
+        return output
+
+    @router.get("/tags", response_model=T.List[str])
+    def get_tags(self):
+        return CLASSES
+
+    @staticmethod
+    def imread(image):
+        try:
+            image = Image.open(image.file).convert("RGB")
+        except Exception:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                detail=f"""{image.filename} is not image file  """,
+            )
+        return image
